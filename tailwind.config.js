75e21099b2ea3c40b7aae0714b9047ad2b974a69
Greg Marine <gregmarine@hey.com>
2023-10-10 09:18:57 -0400

    Adding TailwindCSS support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./.vitepress/theme/components/*.vue",
+    "./**/*.md",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
